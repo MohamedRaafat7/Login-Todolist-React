d391edded692807436f10d77b985e64924b15714
qweasdf1231 <mraft5868@gmail.com>
2024-10-12 14:58:21 +0300

    first commit

---
+++ LoginReact/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import './App.css';
+
+export default function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <nav>
+          <ul>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+           
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        
+        </Routes>
+      </div>
+    </Router>
+  );
+}
---
+++ Todolist-react/src/App.jsx
@@ -0,0 +1,49 @@
+
+import "./App.css";
+import { useState, useRef } from "react";
+
+export default function MyApp() {
+  const deviceInputRef = useRef(null); 
+  const [devices, setDevices] = useState(["mac", "oppo", "iphone"]); 
+
+  const handleAddDevice = () => {
+    const newDevice = deviceInputRef.current.value;
+    if (newDevice) {      //  check not empty
+      setDevices([...devices, newDevice]); 
+      deviceInputRef.current.value = ""; 
+    }
+  };
+
+  const handleDeleteDevice = (indexToDelete) => {
+    setDevices(devices.filter((device, index) => index !== indexToDelete));
+  };
+
+  const devicesList = devices.map((device, index) => (
+    <li key={index}>
+      {device}
+      <button onClick={() => handleDeleteDevice(index)}>Delete</button>
+    </li>
+  ));
+
+  return (
+    <div className="div1">
+      <h1 className="h1">To-Do List</h1>
+      <ul>{devicesList}</ul>
+      <label htmlFor="deviceInput" >Add Device</label>
+      <input
+        id="deviceInput"
+        ref={deviceInputRef} 
+        type="text"
+        placeholder="Enter Device Name ..."
+      />
+      <button onClick={handleAddDevice}>Add</button>
+    </div>
+  );
+}  
+
+
+
+
+
+
+
